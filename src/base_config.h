0d3b135870712e3de4dd5005fadfcb5290fe3916
swapnil <swapnil@swapnil.(none)>
2014-03-21 04:19:13 +0530

    Added field UTIL_END

--- src/base_config.h
+++ src/base_config.h
@@ -2,17 +2,18 @@
 #define HEADER_BASE_CONFIG
 
 // BASE CONFIG
-#define NUM_TASKS 4
+#define NUM_TASKS 8
 #define CACHE_SIZE 256
 #define RF 0.8 			// Some form of Reuse Factor for Cache
 #define Total_ECBs_CU 10
-#define NUM_TASK_SETS 500
+#define NUM_TASK_SETS 10
 
 double BRT = 8e-3;		// In MicroSecs
 double NIPMS = 0.04;		// Number of Insns per Micro Second
 
-#define UTIL_START 0.65
-#define UTIL_INCREMENT 0.05
+#define UTIL_START 0.5
+#define UTIL_INCREMENT 0.01
+#define UTIL_END 0.7
 
 #define MIN_PERIOD 20
 #define MAX_PERIOD 500
