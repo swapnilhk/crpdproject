0cfae2830f652c8a5f905997f405caa4ceb477bf
swapnil <swapnil@swapnil.(none)>
2014-03-26 04:24:35 +0530

    Added 'if(MESSAGE_LEVEL > NONE)' before all fclose() statements

--- src/kd.cpp
+++ src/kd.cpp
@@ -383,7 +383,7 @@ int Response_time_PRE_MAX_KD(){
 	if(sched)
 		Num_Executed_Tasks[PRE_MAX_KD]++;
 
-	if(fp != NULL)
+	if(MESSAGE_LEVEL > NONE && fp != NULL)
 		fclose(fp);
 	
 	return sched ? 1 : 0;
